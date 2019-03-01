85a2378160b4f6d92c0499e7b5d96b45bd003db5
kozhinamasha <kozhinamasha@gmail.com>
2019-03-01 13:42:13 +0200

    Get Git Repositories using DataContractJsonSerializer

---
+++ GetGitRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace ApiClients
+{
+    class Program
+    {
+        private static readonly  HttpClient Client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            ProcessGitRepositories().Wait();
+            var repositories = ProcessGitRepositories().Result;
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine(repo.LastUpdated);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        private static async Task <List<Repository>> ProcessGitRepositories()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            Client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            var streamTask = Client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            return repositories;
+
+            //Task<string> stringTask = _client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            //var msg = await stringTask;
+            //Console.Write(msg);
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace ApiClients
+{
+    [DataContract(Name = "repo")]
+    public class Repository
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name = "homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
+
+        [DataMember(Name = "pushed_at")]
+        private string PushedAt { get; set; }
+
+        [DataMember (Name="updated_at")]
+        private string Updated { get; set; }
+
+        [IgnoreDataMember]
+        public DateTime LastPush
+        {
+            get
+            {
+                return DateTime.ParseExact(PushedAt, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            }
+        }
+
+        [IgnoreDataMember]
+        public DateTime LastUpdated
+        {
+            get { return DateTime.ParseExact(Updated, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);}
+        }
+    }
+}
